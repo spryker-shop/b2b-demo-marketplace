5d21514eaf2cbe253ff459c9eb83ba8596f43fe6
Nikita Peretyatko <peretyatko@logema.org>
2022-02-04 17:20:57 +0300

    MP-6394: Glue API: Marketplace Product + Cart feature Marketplace Product + Inventory Management

---
+++ src/Pyz/Zed/AvailabilityGui/AvailabilityGuiDependencyProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\AvailabilityGui;
+
+use Spryker\Zed\AvailabilityGui\AvailabilityGuiDependencyProvider as SprykerAvailabilityGuiDependencyProvider;
+use Spryker\Zed\MerchantProductGui\Communication\Plugin\AvailabilityGui\MerchantProductAvailabilityAbstractTableQueryCriteriaExpanderPlugin;
+use Spryker\Zed\MerchantProductGui\Communication\Plugin\AvailabilityGui\MerchantProductAvailabilityViewActionViewDataExpanderPlugin;
+
+class AvailabilityGuiDependencyProvider extends SprykerAvailabilityGuiDependencyProvider
+{
+    /**
+     * @return \Spryker\Zed\AvailabilityGuiExtension\Dependency\Plugin\AvailabilityViewActionViewDataExpanderPluginInterface[]
+     */
+    protected function getAvailabilityViewActionViewDataExpanderPlugins(): array
+    {
+        return [
+            new MerchantProductAvailabilityViewActionViewDataExpanderPlugin(),
+        ];
+    }
+
+    /**
+     * @return \Spryker\Zed\AvailabilityGuiExtension\Dependency\Plugin\AvailabilityAbstractTableQueryCriteriaExpanderPluginInterface[]
+     */
+    protected function getAvailabilityAbstractTableQueryCriteriaExpanderPlugins(): array
+    {
+        return [
+            new MerchantProductAvailabilityAbstractTableQueryCriteriaExpanderPlugin(),
+        ];
+    }
+}
