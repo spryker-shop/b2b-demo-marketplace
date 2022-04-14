67a4967dc96a709fe0768333a2bc9398f9e97479
Ilya Antipov <antipov.i-dev@adict.ru>
2022-04-14 18:12:37 +0300

    MP-6741: Fix product offer availability

---
+++ src/Pyz/Zed/AvailabilityCartConnector/AvailabilityCartConnectorDependencyProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\AvailabilityCartConnector;
+
+use Spryker\Zed\AvailabilityCartConnector\AvailabilityCartConnectorDependencyProvider as SprykerAbstractBundleDependencyProvider;
+use Spryker\Zed\ProductConfigurationCart\Communication\Plugin\AvailabilityCartConnector\ProductConfigurationCartItemQuantityCounterStrategyPlugin;
+use Spryker\Zed\ProductOffer\Communication\Plugin\Cart\ProductOfferCartItemQuantityCounterStrategyPlugin;
+
+class AvailabilityCartConnectorDependencyProvider extends SprykerAbstractBundleDependencyProvider
+{
+    /**
+     * @return array<\Spryker\Zed\AvailabilityCartConnectorExtension\Dependency\Plugin\CartItemQuantityCounterStrategyPluginInterface>
+     */
+    public function getCartItemQuantityCounterStrategyPlugins(): array
+    {
+        return [
+            new ProductOfferCartItemQuantityCounterStrategyPlugin(),
+        ];
+    }
+}
