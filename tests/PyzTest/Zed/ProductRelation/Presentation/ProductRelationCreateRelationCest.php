e0091678dd3a59e71cba824cb19942529f4f8e20
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2022-11-19 00:35:28 +0100

    ProductRelationCreateRelationCest fix

--- tests/PyzTest/Zed/ProductRelation/Presentation/ProductRelationCreateRelationCest.php
+++ tests/PyzTest/Zed/ProductRelation/Presentation/ProductRelationCreateRelationCest.php
@@ -37,10 +37,9 @@ class ProductRelationCreateRelationCest
         $i->wantTo('I want to create up selling relation');
         $i->expect('relation is persisted, exported to yves and carousel component is visible');
 
-        $i->amLoggedInUser();
-
         $i->amOnPage(ProductRelationCreatePage::URL);
 
+        $i->waitForElement('//*[@id="product_relation_productRelationKey"]');
         $i->fillField('//*[@id="product_relation_productRelationKey"]', uniqid('key-', false));
         $i->filterProductsByName(ProductRelationCreatePage::PRODUCT_RELATION_PRODUCT_1_NAME);
 
