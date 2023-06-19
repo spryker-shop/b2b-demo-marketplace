238513bd7d33a00e7abb68aa543c0bf8c28f285e
Bohdan Turchyk <bohdan.turchyk@spryker.com>
2023-06-19 11:45:24 +0300

    PBC-1611 Temporary fix to avoid oauth issues

--- config/Shared/config_default.php
+++ config/Shared/config_default.php
@@ -247,8 +247,8 @@ $config[OauthConstants::PUBLIC_KEY_PATH]
         getenv('SPRYKER_OAUTH_KEY_PUBLIC') ?: '',
     ) ?: null;
 $config[OauthConstants::ENCRYPTION_KEY] = getenv('SPRYKER_OAUTH_ENCRYPTION_KEY') ?: null;
-$config[OauthConstants::OAUTH_CLIENT_CONFIGURATION] = json_decode(getenv('SPRYKER_OAUTH_CLIENT_CONFIGURATION'), true) ?: [];
-$config[OauthConstants::OAUTH_CLIENT_CONFIGURATION] = json_decode('{"identifier":"frontend","secret":"U7LBxyH6jfpmgNbbH6jWaMe1SVQiDoH1KSntMeIU74nikbxs","isConfidential":true,"name":"Customer client","redirectUri":null,"isDefault":true}') ?: [];
+// $config[OauthConstants::OAUTH_CLIENT_CONFIGURATION] = json_decode(getenv('SPRYKER_OAUTH_CLIENT_CONFIGURATION'), true) ?: [];
+$config[OauthConstants::OAUTH_CLIENT_CONFIGURATION] = json_decode('[{"identifier":"frontend","secret":"U7LBxyH6jfpmgNbbH6jWaMe1SVQiDoH1KSntMeIU74nikbxs","isConfidential":true,"name":"Customer client","redirectUri":null,"isDefault":true}]') ?: [];
 
 // >> ZED REQUEST
 
