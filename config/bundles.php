b8442a479036e44d544dcae069fe2142ac64fbb6
vol4onok <alexander.veselov@spryker.com>
2024-10-23 09:52:57 +0300

    CC-34696: CC-34695: reverted SF generation

--- config/bundles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    SprykerSdk\Evaluator\EvaluatorBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-];
