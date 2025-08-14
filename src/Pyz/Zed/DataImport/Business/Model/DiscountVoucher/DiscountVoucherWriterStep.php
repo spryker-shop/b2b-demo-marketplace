7839e916055ff0d2b6617147d6d74a2f2a4fe8c5
bohdanyevtukhov <bohdan.yevtukhov@spryker.com>
2025-08-14 18:11:00 +0300

    CC-35970: Flaky CI. Dataimport discount voucher.

--- src/Pyz/Zed/DataImport/Business/Model/DiscountVoucher/DiscountVoucherWriterStep.php
+++ src/Pyz/Zed/DataImport/Business/Model/DiscountVoucher/DiscountVoucherWriterStep.php
@@ -9,6 +9,7 @@ declare(strict_types = 1);
 
 namespace Pyz\Zed\DataImport\Business\Model\DiscountVoucher;
 
+use Exception;
 use Orm\Zed\Discount\Persistence\SpyDiscount;
 use Orm\Zed\Discount\Persistence\SpyDiscountQuery;
 use Orm\Zed\Discount\Persistence\SpyDiscountVoucher;
@@ -109,7 +110,11 @@ class DiscountVoucherWriterStep implements DataImportStepInterface
             $voucherCodeCollection->append($discountVoucherEntity);
         }
 
-        $voucherCodeCollection->save();
+        try {
+            $voucherCodeCollection->save();
+        } catch (Exception) {
+            throw new Exception('Failed to save. Data:' . json_encode($voucherCodeCollection->toArray()));
+        }
     }
 
     /**
