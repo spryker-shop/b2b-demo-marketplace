d51423096d2966013aaad537016126aa3a56db21
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2025-09-25 16:02:15 +0200

    MySQl storage reader fixes

--- src/Pyz/Client/StorageDatabase/Storage/Reader/MySqlStorageReader.php
+++ src/Pyz/Client/StorageDatabase/Storage/Reader/MySqlStorageReader.php
@@ -11,10 +11,78 @@ class MySqlStorageReader extends SprykerMySqlStorageReader
      * @var string
      */
     protected const SELECT_STATEMENT_PATTERN = '
-      SELECT %1$s as resource_key, data AS resource_data
+      SELECT %1$s as resource_key, data AS resource_data, `key` AS key_column, `alias_keys` AS alias_keys_column
         FROM %3$s
-        WHERE `key` = %1$s
+        WHERE `key` = %1$s OR `alias_keys` = %1$s
     ';
+
+    /**
+     * @param string $resourceKey
+     *
+     * @return string
+     */
+    public function get(string $resourceKey): string
+    {
+        $statement = $this->createSingleSelectStatementForResourceKey($resourceKey);
+        $statement->execute();
+
+        return $this->fetchSingleResult($statement, $resourceKey);
+    }
+
+    /**
+     * @param \Propel\Runtime\Connection\StatementInterface $statement
+     *
+     * @return string
+     */
+    protected function fetchSingleResult(StatementInterface $statement, string $resourceKey = ''): string
+    {
+        $result = $statement->fetch();
+
+        if (isset($result['alias_keys_column']) && $result['alias_keys_column'] === $resourceKey) {
+            $explode = explode(':', $result['key_column']);
+            $id = end($explode);
+            return json_encode(['id' => (int)$id]);
+        }
+
+        return $result['resource_data'] ?? '';
+    }
+
+    /**
+     * @param \Propel\Runtime\Connection\StatementInterface $statement
+     * @param array<string> $resourceKeys
+     *
+     * @return array
+     */
+    protected function fetchMultiResults(StatementInterface $statement, array $resourceKeys): array
+    {
+        $results = $statement->fetchAll();
+        $formattedResults = [];
+
+        if (!$results) {
+            return $formattedResults;
+        }
+
+        foreach ($results as $result) {
+            $resourceKey = $result['resource_key'] ?? null;
+            $resourceData = $result['resource_data'] ?? null;
+
+            if (!$resourceKey || !$resourceData) {
+                continue;
+            }
+
+            if (isset($result['alias_keys_column']) && $result['alias_keys_column'] === $result['resource_key']) {
+                $explode = explode(':', trim($result['key_column']));
+                $id = end($explode);
+                $formattedResults[$resourceKey] = json_encode(['id' => (int)$id]);
+                continue;
+            }
+
+            $formattedResults[$resourceKey] = $resourceData;
+        }
+
+        return $formattedResults;
+    }
+
 
     /**
      * @param array<string> $resourceKeys
@@ -53,10 +121,6 @@ class MySqlStorageReader extends SprykerMySqlStorageReader
 
         foreach ($queryDataPerTable as $tableName => $tableQueryData) {
             if (count($tableQueryData)>1) {
-                $query = "
-      SELECT `key` as resource_key, `data` AS resource_data
-        FROM $tableName
-        WHERE `key` IN ( ";
                 $keys = [];
                 foreach ($tableQueryData as $dataSet) {
                     /**
@@ -67,6 +131,16 @@ class MySqlStorageReader extends SprykerMySqlStorageReader
                     $keys[] = $keyPlaceholder;
                     $queryData[$tableName][][$keyPlaceholder] = $dataSet[$keyPlaceholder];
                 }
+                $query = "
+      SELECT `key` as resource_key, `data` AS resource_data, `key` AS key_column, `alias_keys` AS alias_keys_column
+        FROM $tableName
+        WHERE `key` IN ( ";
+                $query .= implode(',', $keys) . ' )';
+                $selectFragments[] = $query;
+                $query = "
+      SELECT `alias_keys` as resource_key, `data` AS resource_data, `key` AS key_column, `alias_keys` AS alias_keys_column
+        FROM $tableName
+        WHERE `alias_keys` IN ( ";
                 $query .= implode(',', $keys) . ' )';
                 $selectFragments[] = $query;
                 continue;
