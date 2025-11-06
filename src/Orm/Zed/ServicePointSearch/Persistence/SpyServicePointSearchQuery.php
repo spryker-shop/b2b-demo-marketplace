4545786eeff973101c6c19f0209a4652530652ef
pivovarov <stanislav.pivovarov@spryker.com>
2025-11-06 15:41:39 +0100

    add ssp composer dependencies

---
+++ src/Orm/Zed/ServicePointSearch/Persistence/SpyServicePointSearchQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\ServicePointSearch\Persistence;
+
+use Spryker\Zed\ServicePointSearch\Persistence\Propel\AbstractSpyServicePointSearchQuery as BaseSpyServicePointSearchQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_service_point_search' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyServicePointSearchQuery extends BaseSpyServicePointSearchQuery
+{
+
+}
