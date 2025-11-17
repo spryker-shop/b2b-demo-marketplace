a3a6e70c75d97e5783f4a3acd6866c14991c54f9
pivovarov <stanislav.pivovarov@spryker.com>
2025-11-17 17:20:22 +0100

    add config for remote deployment

---
+++ config/Shared/config_default-docker.ssp.b2b_mp.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types = 1);
+
+use Spryker\Service\FlysystemAws3v3FileSystem\Plugin\Flysystem\Aws3v3FilesystemBuilderPlugin;
+use Spryker\Shared\FileSystem\FileSystemConstants;
+
+require 'common/config_oms-development.php';
+
+// >>> FILESYSTEM
+$config[FileSystemConstants::FILESYSTEM_SERVICE] = [
+    'ssp-inquiry' => [
+        'sprykerAdapterClass' => Aws3v3FilesystemBuilderPlugin::class,
+        'key' => getenv('TEST_BUCKET_6_SSP_CLAIM_STORAGE_S3_KEY_ACTUAL') ?: '',
+        'secret' => getenv('TEST_BUCKET_6_SSP_CLAIM_STORAGE_S3_SECRET_ACTUAL') ?: '',
+        'bucket' => 'shared-01-11-scos-bucket-ssp-claim-storage',
+        'region' => getenv('AWS_REGION'),
+        'version' => 'latest',
+        'root' => '/ssp-inquiry',
+        'path' => '',
+    ],
+    'files' => [
+        'sprykerAdapterClass' => Aws3v3FilesystemBuilderPlugin::class,
+        'key' => getenv('TEST_BUCKET_6_SSP_FILES_STORAGE_S3_KEY_ACTUAL') ?: '',
+        'secret' => getenv('TEST_BUCKET_6_SSP_FILES_STORAGE_S3_SECRET_ACTUAL') ?: '',
+        'bucket' => 'shared-01-11-scos-bucket-ssp-files-storage',
+        'region' => getenv('AWS_REGION'),
+        'version' => 'latest',
+        'root' => '/files',
+        'path' => '',
+    ],
+    'ssp-asset-image' => [
+        'sprykerAdapterClass' => Aws3v3FilesystemBuilderPlugin::class,
+        'key' => getenv('TEST_BUCKET_6_SSP_ASSETS_STORAGE_S3_KEY_ACTUAL') ?: '',
+        'secret' => getenv('TEST_BUCKET_6_SSP_ASSETS_STORAGE_S3_SECRET_ACTUAL') ?: '',
+        'bucket' => 'shared-01-11-scos-bucket-ssp-assets-storage',
+        'region' => getenv('AWS_REGION'),
+        'version' => 'latest',
+        'root' => '/ssp-asset-image',
+        'path' => '',
+    ],
+    'ssp-model-image' => [
+        'sprykerAdapterClass' => Aws3v3FilesystemBuilderPlugin::class,
+        'key' => getenv('TEST_BUCKET_6_SSP_MODELS_STORAGE_S3_KEY_ACTUAL') ?: '',
+        'secret' => getenv('TEST_BUCKET_6_SSP_MODELS_STORAGE_S3_SECRET_ACTUAL') ?: '',
+        'bucket' => 'shared-01-11-scos-bucket-ssp-models-storage',
+        'region' => getenv('AWS_REGION'),
+        'version' => 'latest',
+        'root' => '/ssp-model-image',
+        'path' => '',
+    ],
+];
