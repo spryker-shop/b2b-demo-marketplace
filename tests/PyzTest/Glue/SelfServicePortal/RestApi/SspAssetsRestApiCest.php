f22111b6bca25223d00d8a4cb1974b3a7c90bbee
annakotiuk <anna.kotiuk@spryker.com>
2025-12-09 13:01:56 +0100

    CC-36695: fix tests

--- tests/PyzTest/Glue/SelfServicePortal/RestApi/SspAssetsRestApiCest.php
+++ tests/PyzTest/Glue/SelfServicePortal/RestApi/SspAssetsRestApiCest.php
@@ -23,7 +23,6 @@ use PyzTest\Glue\SelfServicePortal\SelfServicePortalApiTester;
  * @group RestApi
  * @group SspAssetsRestApiCest
  * Add your own group annotations below this line
- * @group EndToEnd
  */
 class SspAssetsRestApiCest
 {
