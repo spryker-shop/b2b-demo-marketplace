29ae4895d6521f1348625f79b264315dd5ebff31
denyssokolov <denys.sokolov@spryker.com>
2025-12-12 10:46:52 +0100

    CC-36535: style fixes.

--- src/Pyz/Yves/SelfServicePortal/Theme/default/components/molecules/service-point-shipment-types/service-point-shipment-types.ts
+++ src/Pyz/Yves/SelfServicePortal/Theme/default/components/molecules/service-point-shipment-types/service-point-shipment-types.ts
@@ -1,4 +1,6 @@
-import CoreServicePointShipmentTypes, { EVENT_SHIPMENT_TYPE_CHANGE } from 'SelfServicePortal/components/molecules/service-point-shipment-types/service-point-shipment-types';
+import CoreServicePointShipmentTypes, {
+    EVENT_SHIPMENT_TYPE_CHANGE,
+} from 'SelfServicePortal/components/molecules/service-point-shipment-types/service-point-shipment-types';
 
 export default class ServicePointShipmentTypes extends CoreServicePointShipmentTypes {
     protected toggle(event: Event): void {
--- src/Pyz/Yves/SelfServicePortal/Theme/default/components/molecules/ssp-service-point-selector/ssp-service-point-selector.ts
+++ src/Pyz/Yves/SelfServicePortal/Theme/default/components/molecules/ssp-service-point-selector/ssp-service-point-selector.ts
@@ -34,7 +34,7 @@ export default class SspServicePointSelector extends CoreSspServicePointSelector
                 } else {
                     this.changePriceVisibility('');
                 }
-            })
+            });
         });
     }
 }
