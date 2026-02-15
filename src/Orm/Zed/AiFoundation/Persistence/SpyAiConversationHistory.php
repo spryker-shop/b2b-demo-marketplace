86a07917d156a1ebf138e454f2a0543f1b557037
vitaliiivanov <vitalii.ivanov@spryker.com>
2026-02-15 11:54:48 +0200

    ai-30 `.gitignore` to exclude `.nx` directory and remove obsolete Nx daemon cache logs

---
+++ src/Orm/Zed/AiFoundation/Persistence/SpyAiConversationHistory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\AiFoundation\Persistence;
+
+use Orm\Zed\AiFoundation\Persistence\Base\SpyAiConversationHistory as BaseSpyAiConversationHistory;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_ai_conversation_history' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyAiConversationHistory extends BaseSpyAiConversationHistory
+{
+
+}
