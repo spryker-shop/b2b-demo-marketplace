7d8df211b60104e404f757a1b9ba82e153e16926
vitaliiivanov <vitalii.ivanov@spryker.com>
2026-02-26 19:37:37 +0200

    Integrate `WebProfilerPropelDataCollectorPlugin` into `WebProfilerDependencyProvider` for enhanced Propel profiling in Zed.

--- src/Pyz/Zed/WebProfiler/WebProfilerDependencyProvider.php
+++ src/Pyz/Zed/WebProfiler/WebProfilerDependencyProvider.php
@@ -9,9 +9,9 @@ declare(strict_types = 1);
 
 namespace Pyz\Zed\WebProfiler;
 
-use Spryker\Glue\Propel\Plugin\WebProfiler\WebProfilerPropelDataCollectorPlugin;
 use Spryker\Zed\Config\Communication\Plugin\WebProfiler\WebProfilerConfigDataCollectorPlugin;
 use Spryker\Zed\Profiler\Communication\Plugin\WebProfiler\WebProfilerProfilerDataCollectorPlugin;
+use Spryker\Zed\Propel\Communication\Plugin\WebProfiler\WebProfilerPropelDataCollectorPlugin;
 use Spryker\Zed\WebProfiler\Communication\Plugin\WebProfiler\WebProfilerAjaxDataCollectorPlugin;
 use Spryker\Zed\WebProfiler\Communication\Plugin\WebProfiler\WebProfilerConfigDataCollectorPlugin as SymfonyWebProfilerConfigDataCollectorPlugin;
 use Spryker\Zed\WebProfiler\Communication\Plugin\WebProfiler\WebProfilerEventsDataCollectorPlugin;
