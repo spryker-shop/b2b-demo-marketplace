99c6be93fb5376a129feeb9b11e043ecf3fa0103
vitaliiivanov <vitalii.ivanov@spryker.com>
2026-03-04 15:28:08 +0200

    Update AI configuration to use `OPEN_AI_API_TOKEN` instead of `OPENAI_API_KEY`

--- config/Shared/config_ai.php
+++ config/Shared/config_ai.php
@@ -8,7 +8,7 @@ use Spryker\Shared\AiFoundation\AiFoundationConstants;
 $openAiConfiguration = [
     'provider_name' => AiFoundationConstants::PROVIDER_OPENAI,
     'provider_config' => [
-        'key' => getenv('OPENAI_API_KEY') ?: '',
+        'key' => getenv('OPEN_AI_API_TOKEN') ?: '',
         'model' => 'gpt-4o',
     ],
 ];
