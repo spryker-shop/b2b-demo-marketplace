4bd9733626de5d7271c7b41410325a500d27d449
annakotiuk <anna.kotiuk@spryker.com>
2026-03-09 12:34:45 +0100

    CC-37318: CR fixes

--- src/Pyz/Yves/ContentProductWidget/ContentProductWidgetFactory.php
+++ src/Pyz/Yves/ContentProductWidget/ContentProductWidgetFactory.php
@@ -9,17 +9,19 @@ declare(strict_types = 1);
 
 namespace Pyz\Yves\ContentProductWidget;
 
-use Pyz\Yves\ContentProductWidget\Reader\ContentProductAbstractReader;
 use Pyz\Yves\ContentProductWidget\Twig\ContentProductAbstractListTwigFunctionProvider;
-use Spryker\Client\ProductCategoryStorage\ProductCategoryStorageClientInterface;
-use Spryker\Client\Store\StoreClientInterface;
 use Spryker\Shared\Twig\TwigFunctionProvider;
 use SprykerShop\Yves\ContentProductWidget\ContentProductWidgetFactory as SprykerShopContentProductWidgetFactory;
-use SprykerShop\Yves\ContentProductWidget\Reader\ContentProductAbstractReaderInterface;
 use Twig\Environment;
 
 class ContentProductWidgetFactory extends SprykerShopContentProductWidgetFactory
 {
+    /**
+     * @param \Twig\Environment $twig
+     * @param string $localeName
+     *
+     * @return \Spryker\Shared\Twig\TwigFunctionProvider
+     */
     public function createContentProductAbstractListTwigFunctionProvider(Environment $twig, string $localeName): TwigFunctionProvider
     {
         return new ContentProductAbstractListTwigFunctionProvider(
@@ -27,25 +29,5 @@ class ContentProductWidgetFactory extends SprykerShopContentProductWidgetFactory
             $localeName,
             $this->createContentProductAbstractReader(),
         );
-    }
-
-    public function createContentProductAbstractReader(): ContentProductAbstractReaderInterface
-    {
-        return new ContentProductAbstractReader(
-            $this->getContentProductClient(),
-            $this->getProductStorageClient(),
-            $this->getProductCategoryStorageClient(),
-            $this->getStoreClient(),
-        );
-    }
-
-    public function getProductCategoryStorageClient(): ProductCategoryStorageClientInterface
-    {
-        return $this->getProvidedDependency(ContentProductWidgetDependencyProvider::CLIENT_PRODUCT_CATEGORY_STORAGE);
-    }
-
-    public function getStoreClient(): StoreClientInterface
-    {
-        return $this->getProvidedDependency(ContentProductWidgetDependencyProvider::CLIENT_STORE);
     }
 }
