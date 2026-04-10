594887e6c3f1413753d3ddc533cfcfc2b6490f3b
Veronika Golska <veronica.golska@spryker.com>
2026-04-10 08:35:39 +0200

    CC-37778 ai visual searhc integration

--- src/Pyz/Yves/ShopApplication/ShopApplicationDependencyProvider.php
+++ src/Pyz/Yves/ShopApplication/ShopApplicationDependencyProvider.php
@@ -29,6 +29,7 @@ use Spryker\Yves\Sitemap\Widget\SitemapWidget;
 use Spryker\Yves\Translator\Plugin\Application\TranslatorApplicationPlugin;
 use Spryker\Yves\Twig\Plugin\Application\TwigApplicationPlugin;
 use Spryker\Yves\Validator\Plugin\Application\ValidatorApplicationPlugin;
+use SprykerFeature\Yves\AiCommerce\SearchByImage\Widget\ImageSearchAiWidget;
 use SprykerFeature\Yves\BuyBox\Widget\BuyBoxWidget;
 use SprykerFeature\Yves\ProductExperienceManagement\Widget\ProductAttributeVisibilityCartWidget;
 use SprykerFeature\Yves\ProductExperienceManagement\Widget\ProductAttributeVisibilityPdpWidget;
@@ -436,6 +437,7 @@ class ShopApplicationDependencyProvider extends SprykerShopApplicationDependency
             ProductAttributeVisibilityPdpWidget::class,
             ProductAttributeVisibilityPlpWidget::class,
             ProductAttributeVisibilityCartWidget::class,
+            ImageSearchAiWidget::class,
         ];
     }
 
