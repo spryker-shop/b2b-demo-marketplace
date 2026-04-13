0bf033afc816fbbfd489e73a872295b850302de1
Aleksey Belan <alexey.belan@spryker.com>
2026-04-13 13:07:27 +0100

    format

--- src/Pyz/Yves/MultiCartWidget/Theme/default/components/molecules/header-cart-pill/header-cart-pill.ts
+++ src/Pyz/Yves/MultiCartWidget/Theme/default/components/molecules/header-cart-pill/header-cart-pill.ts
@@ -1,3 +1,3 @@
 import HeaderNavPill from '../../../../../../ShopUi/Theme/default/components/molecules/header-nav-pill/header-nav-pill';
 
-export default class HeaderCartPill extends HeaderNavPill { }
+export default class HeaderCartPill extends HeaderNavPill {}
