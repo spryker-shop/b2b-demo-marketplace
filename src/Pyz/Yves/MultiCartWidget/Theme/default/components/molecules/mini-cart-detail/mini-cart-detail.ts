051f313d280a87a90efdf5e3f4c1b6454a437527
Aleksey Belan <alexey.belan@spryker.com>
2026-04-14 18:04:29 +0100

    active cart

--- src/Pyz/Yves/MultiCartWidget/Theme/default/components/molecules/mini-cart-detail/mini-cart-detail.ts
+++ src/Pyz/Yves/MultiCartWidget/Theme/default/components/molecules/mini-cart-detail/mini-cart-detail.ts
@@ -3,12 +3,21 @@ import Component from 'ShopUi/models/component';
 export default class MiniCartDetail extends Component {
     protected linkEl: HTMLElement;
     protected expandBtn: HTMLElement;
+    protected activateForm: HTMLFormElement;
 
     protected readyCallback(): void {}
 
     protected init(): void {
         this.linkEl = this.querySelector<HTMLElement>(`.${this.jsName}__link`);
         this.expandBtn = this.querySelector<HTMLElement>(`.${this.jsName}__expand-btn`);
+        this.activateForm = this.querySelector<HTMLFormElement>(`.${this.jsName}__activate-form`);
+
+        if (this.activateForm && this.linkEl) {
+            this.linkEl.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.activateForm.submit();
+            });
+        }
 
         if (this.expandBtn) {
             this.expandBtn.addEventListener('click', (e) => {
