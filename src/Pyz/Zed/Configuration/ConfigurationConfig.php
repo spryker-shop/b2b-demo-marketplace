843bc54080b445afe807b68e3a3cddda6c8ec404
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2026-04-23 17:25:58 +0200

    CC-37916: Code style fixes

--- src/Pyz/Zed/Configuration/ConfigurationConfig.php
+++ src/Pyz/Zed/Configuration/ConfigurationConfig.php
@@ -1,10 +1,18 @@
 <?php
 
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
 namespace Pyz\Zed\Configuration;
 
 use Generated\Shared\Transfer\DataImporterDataSourceConfigurationTransfer;
+use Spryker\Zed\Configuration\ConfigurationConfig as SprykerConfigurationConfig;
 
-class ConfigurationConfig extends \Spryker\Zed\Configuration\ConfigurationConfig
+class ConfigurationConfig extends SprykerConfigurationConfig
 {
     /**
      * Specification:
