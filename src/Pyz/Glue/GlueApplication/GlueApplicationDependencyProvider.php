9b16fc4bfc8c8bb083faa7845a16ee50e1173b61
Olha Livitchuk <olha.livitchuk@spryker.com>
2026-05-18 18:09:15 +0200

    CC-37806 Fixed routes

--- src/Pyz/Glue/GlueApplication/GlueApplicationDependencyProvider.php
+++ src/Pyz/Glue/GlueApplication/GlueApplicationDependencyProvider.php
@@ -76,7 +76,6 @@ use Spryker\Glue\EventDispatcher\Plugin\Application\EventDispatcherApplicationPl
 use Spryker\Glue\GlueApplication\GlueApplicationDependencyProvider as SprykerGlueApplicationDependencyProvider;
 use Spryker\Glue\GlueApplication\Plugin\Application\GlueApplicationApplicationPlugin;
 use Spryker\Glue\GlueApplication\Plugin\GlueApplication\CorsValidateHttpRequestPlugin;
-use Spryker\Glue\GlueApplication\Plugin\GlueApplication\FallbackStorefrontApiGlueApplicationBootstrapPlugin;
 use Spryker\Glue\GlueApplication\Plugin\GlueApplication\HeadersValidateHttpRequestPlugin;
 use Spryker\Glue\GlueApplication\Plugin\GlueApplication\PaginationParametersValidateHttpRequestPlugin;
 use Spryker\Glue\GlueApplicationAuthorizationConnector\Plugin\GlueApplication\AuthorizationRestUserValidatorPlugin;
@@ -89,12 +88,6 @@ use Spryker\Glue\GlueBackendApiApplication\Plugin\GlueApplication\CustomRouteRou
 use Spryker\Glue\GlueBackendApiApplication\Plugin\GlueApplication\ResourcesProviderPlugin as BackendResourcesProviderPlugin;
 use Spryker\Glue\GlueBackendApiApplicationAuthorizationConnector\Plugin\GlueApplication\IsProtectedTableColumnExpanderPlugin as BackendIsProtectedTableColumnExpanderPlugin;
 use Spryker\Glue\GlueJsonApiConvention\Plugin\GlueApplication\JsonApiConventionPlugin;
-use Spryker\Glue\GlueStorefrontApiApplication\Plugin\GlueApplication\ControllerConfigurationCacheCollectorPlugin as StorefrontControllerConfigurationCacheCollectorPlugin;
-use Spryker\Glue\GlueStorefrontApiApplication\Plugin\GlueApplication\CustomRouteRoutesProviderPlugin as StorefrontCustomRouteRoutesProviderPlugin;
-use Spryker\Glue\GlueStorefrontApiApplication\Plugin\GlueApplication\ResourcesProviderPlugin as StorefrontResourcesProviderPlugin;
-use Spryker\Glue\GlueStorefrontApiApplication\Plugin\GlueApplication\StorefrontApiGlueApplicationBootstrapPlugin;
-use Spryker\Glue\GlueStorefrontApiApplication\Plugin\GlueApplication\StorefrontRouterProviderPlugin;
-use Spryker\Glue\GlueStorefrontApiApplicationAuthorizationConnector\Plugin\GlueApplication\IsProtectedTableColumnExpanderPlugin as StorefrontIsProtectedTableColumnExpanderPlugin;
 use Spryker\Glue\HealthCheck\Plugin\HealthCheckResourceRoutePlugin;
 use Spryker\Glue\Http\Plugin\Application\HttpApplicationPlugin;
 use Spryker\Glue\Locale\Plugin\Application\LocaleApplicationPlugin;
@@ -724,9 +717,7 @@ class GlueApplicationDependencyProvider extends SprykerGlueApplicationDependency
     protected function getGlueApplicationBootstrapPlugins(): array
     {
         return [
-            new StorefrontApiGlueApplicationBootstrapPlugin(),
             new BackendApiGlueApplicationBootstrapPlugin(),
-            new FallbackStorefrontApiGlueApplicationBootstrapPlugin(),
         ];
     }
 
@@ -746,7 +737,6 @@ class GlueApplicationDependencyProvider extends SprykerGlueApplicationDependency
     protected function getControllerConfigurationCacheCollectorPlugins(): array
     {
         return [
-            new StorefrontControllerConfigurationCacheCollectorPlugin(),
             new BackendControllerConfigurationCacheCollectorPlugin(),
         ];
     }
@@ -758,7 +748,6 @@ class GlueApplicationDependencyProvider extends SprykerGlueApplicationDependency
     {
         return [
             new BackendRouterProviderPlugin(),
-            new StorefrontRouterProviderPlugin(),
         ];
     }
 
@@ -768,7 +757,6 @@ class GlueApplicationDependencyProvider extends SprykerGlueApplicationDependency
     protected function getRoutesProviderPlugins(): array
     {
         return [
-            new StorefrontCustomRouteRoutesProviderPlugin(),
             new BackendCustomRouteRoutesProviderPlugin(),
         ];
     }
@@ -779,7 +767,6 @@ class GlueApplicationDependencyProvider extends SprykerGlueApplicationDependency
     protected function getResourcesProviderPlugins(): array
     {
         return [
-            new StorefrontResourcesProviderPlugin(),
             new BackendResourcesProviderPlugin(),
         ];
     }
@@ -791,7 +778,6 @@ class GlueApplicationDependencyProvider extends SprykerGlueApplicationDependency
     {
         return [
             new BackendIsProtectedTableColumnExpanderPlugin(),
-            new StorefrontIsProtectedTableColumnExpanderPlugin(),
         ];
     }
 }
