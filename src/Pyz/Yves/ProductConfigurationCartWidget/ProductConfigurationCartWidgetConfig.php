ab498be87dbdb2330b2a798579c0efb2e1a3be32
Olha Livitchuk <olha.livitchuk@spryker.com>
2026-06-10 16:30:32 +0200

    CC-38992 Cart and ShoppingList fix

---
+++ src/Pyz/Yves/ProductConfigurationCartWidget/ProductConfigurationCartWidgetConfig.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace Pyz\Yves\ProductConfigurationCartWidget;
+
+use SprykerShop\Yves\ProductConfigurationCartWidget\ProductConfigurationCartWidgetConfig as SprykerProductConfigurationCartWidgetConfig;
+
+class ProductConfigurationCartWidgetConfig extends SprykerProductConfigurationCartWidgetConfig
+{
+    /**
+     * @var string
+     *
+     * @uses \Pyz\Shared\WaterTreatmentConfigurator\WaterTreatmentConfiguratorConfig::WATER_TREATMENT_CONFIGURATOR_KEY
+     */
+    protected const WATER_TREATMENT_CONFIGURATOR_KEY = 'WATER_TREATMENT_CONFIGURATOR';
+
+    /**
+     * @api
+     *
+     * @return array<string>
+     */
+    public function getSupportedConfiguratorKeys(): array
+    {
+        return array_merge(parent::getSupportedConfiguratorKeys(), [
+            static::WATER_TREATMENT_CONFIGURATOR_KEY,
+        ]);
+    }
+}
