f1412eebf6f53168ed2899880760765d00816b60
Aleksey Belan <alexey.belan@spryker.com>
2026-06-12 23:00:25 +0100

    fix list

--- src/Pyz/Yves/ShoppingListWidget/Theme/default/components/molecules/save-to-shopping-list/save-to-shopping-list.ts
+++ src/Pyz/Yves/ShoppingListWidget/Theme/default/components/molecules/save-to-shopping-list/save-to-shopping-list.ts
@@ -3,17 +3,20 @@ import Component from 'ShopUi/models/component';
 export default class SaveToShoppingList extends Component {
     protected static readonly OFFER_RADIO_SELECTOR = 'input[name="product_offer_reference"]:checked';
     protected static readonly OFFER_INPUT_SELECTOR = '.js-shopping-list__form input[name="productOfferReference"]';
+    protected static readonly TRIGGER_SELECTOR = '[data-qa="save-to-shopping-list-trigger"]';
 
-    protected readyCallback(): void {}
+    protected readyCallback(): void { }
 
     protected init(): void {
-        const trigger = this.querySelector<HTMLElement>(`[data-qa="save-to-shopping-list-trigger"]`);
-
-        if (!trigger) {
-            return;
-        }
-
-        trigger.addEventListener('click', () => this.syncOfferReference(), true);
+        document.addEventListener(
+            'click',
+            (event: Event) => {
+                if ((<HTMLElement>event.target).closest(SaveToShoppingList.TRIGGER_SELECTOR)) {
+                    this.syncOfferReference();
+                }
+            },
+            true,
+        );
     }
 
     protected syncOfferReference(): void {
