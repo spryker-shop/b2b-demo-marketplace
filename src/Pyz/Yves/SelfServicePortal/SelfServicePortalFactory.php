0a493e0c9522ff73bb8f5190a495f22d2065b4b5
Anna Kotiuk <anna.kotiuk@spryker.com>
2026-06-18 16:38:16 +0200

    CC-36863: debug

--- src/Pyz/Yves/SelfServicePortal/SelfServicePortalFactory.php
+++ src/Pyz/Yves/SelfServicePortal/SelfServicePortalFactory.php
@@ -9,7 +9,6 @@ declare(strict_types = 1);
 
 namespace Pyz\Yves\SelfServicePortal;
 
-use Pyz\Yves\SelfServicePortal\Service\Handler\SingleAddressPerShipmentTypePreSubmitHandler;
 use SprykerFeature\Yves\SelfServicePortal\SelfServicePortalFactory as SprykerFeatureSelfServicePortalFactory;
 
 class SelfServicePortalFactory extends SprykerFeatureSelfServicePortalFactory
