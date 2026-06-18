046f430b6813d10f6d5bc1d73be2e5e9f5f195fb
Anna Kotiuk <anna.kotiuk@spryker.com>
2026-06-18 16:04:55 +0200

    CC-36863: debug

--- src/Pyz/Yves/SelfServicePortal/SelfServicePortalFactory.php
+++ src/Pyz/Yves/SelfServicePortal/SelfServicePortalFactory.php
@@ -11,12 +11,7 @@ namespace Pyz\Yves\SelfServicePortal;
 
 use Pyz\Yves\SelfServicePortal\Service\Handler\SingleAddressPerShipmentTypePreSubmitHandler;
 use SprykerFeature\Yves\SelfServicePortal\SelfServicePortalFactory as SprykerFeatureSelfServicePortalFactory;
-use SprykerFeature\Yves\SelfServicePortal\Service\Handler\SingleAddressPerShipmentTypePreSubmitHandlerInterface;
 
 class SelfServicePortalFactory extends SprykerFeatureSelfServicePortalFactory
 {
-    public function createSingleAddressPerShipmentTypePreSubmitHandler(): SingleAddressPerShipmentTypePreSubmitHandlerInterface
-    {
-        return new SingleAddressPerShipmentTypePreSubmitHandler($this->createAddressFormChecker());
-    }
 }
