f624ef61e6b29e1b1e3ff26e61bae6ce6d5b405c
Aleksey Belan <alexey.belan@spryker.com>
2026-06-19 13:33:24 +0100

    chankNames

--- src/Pyz/Yves/PriceProductVolumeWidget/Theme/default/components/molecules/volume-price-table/index.ts
+++ src/Pyz/Yves/PriceProductVolumeWidget/Theme/default/components/molecules/volume-price-table/index.ts
@@ -1,3 +1,12 @@
 import './volume-price-table.scss';
 import register from 'ShopUi/app/registry';
-export default register('volume-price-table', () => import(/* webpackMode: "eager" */ './volume-price-table'));
+
+export default register(
+    'volume-price-table',
+    () =>
+        import(
+            /* webpackMode: "eager" */
+            /* webpackChunkName: "volume-price-table" */
+            './volume-price-table'
+        ),
+);
