43b90f9f2de8c01a6e40494ff2e48a8c693e1bfe
Anna Kotiuk <anna.kotiuk@spryker.com>
2026-07-10 17:21:28 +0200

    CC-39522: integrate invoice DoB

---
+++ src/Pyz/Yves/DummyMarketplacePayment/DummyMarketplacePaymentConfig.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace Pyz\Yves\DummyMarketplacePayment;
+
+use Spryker\Yves\DummyMarketplacePayment\DummyMarketplacePaymentConfig as SprykerDummyMarketplacePaymentConfig;
+
+class DummyMarketplacePaymentConfig extends SprykerDummyMarketplacePaymentConfig
+{
+    public function isDateOfBirthEnabled(): bool
+    {
+        return false;
+    }
+}
