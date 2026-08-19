7c645fb1a000c85e527c418a1cde827a8928b2e3
gechetspr <serhii.chepela@spryker.com>
2026-08-19 14:21:44 +0300

    Updated with master

--- src/Pyz/Client/SymfonyScheduler/SymfonySchedulerConfig.php
+++ src/Pyz/Client/SymfonyScheduler/SymfonySchedulerConfig.php
@@ -82,10 +82,6 @@ class SymfonySchedulerConfig extends SprykerSymfonySchedulerConfig
                 'schedule' => '0 * * * *',
                 'priority' => 200,
             ],
-            'recurring-orders-forecast-refresh' => [
-                'command' => $logger . '$PHP_BIN vendor/bin/console recurring-orders:forecast:refresh',
-                'schedule' => '0 * * * *',
-            ],
             'product-relation-updater' => [
                 'command' => $logger . '$PHP_BIN vendor/bin/console product-relation:update -vvv',
                 'schedule' => '30 2 * * *',
