494f8ef5bb11f8432ff2b6c3554898c5c06566de
gechetspr <serhii.chepela@spryker.com>
2026-08-20 09:49:22 +0300

    SC-25088 Added a flasg for SymfonyMessenger and Scheduler

--- config/Zed/cronjobs/jenkins.php
+++ config/Zed/cronjobs/jenkins.php
@@ -2,6 +2,8 @@
 
 declare(strict_types = 1);
 
+use Pyz\Client\SymfonyScheduler\SymfonySchedulerConfig;
+
 /**
  * Notes:
  *
@@ -12,18 +14,31 @@ declare(strict_types = 1);
 //$logger = 'config/Zed/cronjobs/bin/loggable.sh '; // script for jenkins logging
 $logger = '';
 
-$jobs[] = [
-    'name' => 'consume-queue',
-    'command' => $logger . '$PHP_BIN vendor/bin/console symfonymessenger:consume queue-worker-start --time-limit=900',
-    'schedule' => '* * * * *',
-    'enable' => true,
-];
-$jobs[] = [
-    'name' => 'consume-other-cron-jobs',
-    'command' => $logger . '$PHP_BIN vendor/bin/console symfonymessenger:consume compiled-cron-scheduler --time-limit=900 --exclude-from-group=queue-worker-start',
-    'schedule' => '* * * * *',
-    'enable' => true,
-];
+if ((bool)filter_var(getenv('CI_SYMFONY_MESSENGER_OFF'), FILTER_VALIDATE_BOOLEAN)) {
+    // Legacy setup: Symfony Scheduler/Messenger is switched off, so Jenkins has to run every cron job
+    // on its own instead of consuming the compiled Symfony Messenger transports.
+    foreach ((new SymfonySchedulerConfig())->getCronJobs() as $jobName => $cronJob) {
+        $jobs[] = [
+            'name' => $jobName,
+            'command' => $cronJob['command'],
+            'schedule' => $cronJob['schedule'],
+            'enable' => true,
+        ];
+    }
+} else {
+    $jobs[] = [
+        'name' => 'consume-queue',
+        'command' => $logger . '$PHP_BIN vendor/bin/console symfonymessenger:consume queue-worker-start --time-limit=900',
+        'schedule' => '* * * * *',
+        'enable' => true,
+    ];
+    $jobs[] = [
+        'name' => 'consume-other-cron-jobs',
+        'command' => $logger . '$PHP_BIN vendor/bin/console symfonymessenger:consume compiled-cron-scheduler --time-limit=900 --exclude-from-group=queue-worker-start',
+        'schedule' => '* * * * *',
+        'enable' => true,
+    ];
+}
 
 if (getenv('SPRYKER_CURRENT_REGION')) {
     foreach ($jobs as $job) {
