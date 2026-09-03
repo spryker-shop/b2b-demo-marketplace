52b07d1d1d6103ab5104db3a0db5d795aa99c64d
Veronika Golska <veronica.golska@spryker.com>
2026-09-03 17:27:14 +0200

    CC-40218 integration of the service points table in BO

--- src/Pyz/Zed/ServicePoint/ServicePointDependencyProvider.php
+++ src/Pyz/Zed/ServicePoint/ServicePointDependencyProvider.php
@@ -9,96 +9,18 @@ declare(strict_types = 1);
 
 namespace Pyz\Zed\ServicePoint;
 
-use Orm\Zed\ServicePoint\Persistence\SpyServicePointQuery;
-use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
-use Spryker\Zed\Kernel\Container;
-use Spryker\Zed\ServicePoint\Dependency\Facade\ServicePointToCountryFacadeBridge;
-use Spryker\Zed\ServicePoint\Dependency\Facade\ServicePointToStoreFacadeBridge;
+use Spryker\Zed\ProductOfferServicePoint\Communication\Plugin\ServicePoint\ProductOfferServicePointViewSectionPlugin;
+use Spryker\Zed\ServicePoint\ServicePointDependencyProvider as SprykerServicePointDependencyProvider;
 
-/**
- * @method \Spryker\Zed\ServicePoint\ServicePointConfig getConfig()
- */
-class ServicePointDependencyProvider extends AbstractBundleDependencyProvider
+class ServicePointDependencyProvider extends SprykerServicePointDependencyProvider
 {
-    /**
-     * @var string
-     */
-    public const FACADE_STORE = 'FACADE_STORE';
-
-    /**
-     * @var string
-     */
-    public const FACADE_COUNTRY = 'FACADE_COUNTRY';
-
-    public const string PROPEL_QUERY_SERVICE_POINT = 'PROPEL_QUERY_SERVICE_POINT';
-
-    public const string PLUGINS_SERVICE_POINT_VIEW_SECTION = 'PLUGINS_SERVICE_POINT_VIEW_SECTION';
-
-    public function provideBusinessLayerDependencies(Container $container): Container
-    {
-        $container = parent::provideBusinessLayerDependencies($container);
-
-        $container = $this->addStoreFacade($container);
-        $container = $this->addCountryFacade($container);
-
-        return $container;
-    }
-
-    public function provideCommunicationLayerDependencies(Container $container): Container
-    {
-        $container = parent::provideCommunicationLayerDependencies($container);
-
-        $container = $this->addServicePointPropelQuery($container);
-        $container = $this->addServicePointViewSectionPlugins($container);
-
-        return $container;
-    }
-
-    protected function addServicePointPropelQuery(Container $container): Container
-    {
-        $container->set(static::PROPEL_QUERY_SERVICE_POINT, $container->factory(function () {
-            return SpyServicePointQuery::create();
-        }));
-
-        return $container;
-    }
-
-    protected function addServicePointViewSectionPlugins(Container $container): Container
-    {
-        $container->set(static::PLUGINS_SERVICE_POINT_VIEW_SECTION, function () {
-            return $this->getServicePointViewSectionPlugins();
-        });
-
-        return $container;
-    }
-
     /**
      * @return array<\Spryker\Zed\ServicePoint\Dependency\Plugin\ServicePointViewSectionPluginInterface>
      */
     protected function getServicePointViewSectionPlugins(): array
     {
-        return [];
-    }
-
-    protected function addCountryFacade(Container $container): Container
-    {
-        $container->set(static::FACADE_COUNTRY, function (Container $container) {
-            return new ServicePointToCountryFacadeBridge(
-                $container->getLocator()->country()->facade(),
-            );
-        });
-
-        return $container;
-    }
-
-    protected function addStoreFacade(Container $container): Container
-    {
-        $container->set(static::FACADE_STORE, function (Container $container) {
-            return new ServicePointToStoreFacadeBridge(
-                $container->getLocator()->store()->facade(),
-            );
-        });
-
-        return $container;
+        return [
+            new ProductOfferServicePointViewSectionPlugin(),
+        ];
     }
 }
